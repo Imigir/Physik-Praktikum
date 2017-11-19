bd74a5d15b1f3ef576e9d568015d96a386515b85
Imigir <david.rolf@online.de>
2017-11-20 00:13:03 +0100

    Werte

--- 5-Praktikum-V204/scripts/mainscriptw.py
+++ 5-Praktikum-V204/scripts/mainscriptw.py
@@ -48,3 +48,55 @@ import scipy.constants as const
 # params = unp.uarray(params, np.sqrt(np.diag(covar)))
 # makeNewTable([convert((r'$c_\text{1}$',r'$c_\text{2}$',r'$T_{\text{A}1}$',r'$T_{\text{A}2}$',r'$\alpha$',r'$D_1$',r'$D_2$',r'$A_1$',r'$A_2$',r'$A_3$',r'$A_4$'),strFormat),convert(np.array([paramsGes2[0],paramsGes1[0],deltat2*10**6,deltat1*10**6,-paramsDaempfung[0]*2,4.48*10**-6 *paramsGes1[0]/2*10**3, 7.26*10**-6 *paramsGes1[0]/2*10**3, (VierteMessung-2*deltat2*10**6)[0]*10**-6 *1410 /2*10**3, unp.uarray((VierteMessung[1]-VierteMessung[0])*10**-6 *1410 /2*10**3, 0), unp.uarray((VierteMessung[2]-VierteMessung[1])*10**-6 *2500 /2*10**3, 0),unp.uarray((VierteMessung[3]-VierteMessung[2])*10**-6 *1410 /2*10**3, 0)]),unpFormat,[[r'\meter\per\second',"",True],[r'\meter\per\second',"",True],[r'\micro\second',"",True],[r'\micro\second',"",True],[r'\per\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'2.2f',True]]),convert(np.array([2730,2730]),floatFormat,[r'\meter\per\second','1.0f',True])+convert((r'-',r'-'),strFormat)+convert(unp.uarray([57,6.05,9.9],[2.5,0,0]),unpFormat,[[r'\per\meter',"",True],[r'\milli\meter',r'1.2f',True],[r'\milli\meter',r'1.2f',True]])+convert((r'-',r'-',r'-',r'-'),strFormat),convert(np.array([(2730-paramsGes2[0])/2730*100,(2730-paramsGes1[0])/2730*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-'),strFormat)+convert(np.array([(-paramsDaempfung[0]*2-unp.uarray(57,2.5))/unp.uarray(57,2.5)*100,(4.48*10**-6 *paramsGes1[0]/2*10**3-6.05)/6.05*100, (-7.26*10**-6 *paramsGes1[0]/2*10**3+9.90)/9.90*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-',r'-',r'-'),strFormat)],r'{Wert}&{gemessen}&{Literaturwert\cite{cAcryl},\cite{alphaAcryl}}&{Abweichung}','Ergebnisse', ['c ','c',r'c','c'])
 
+
+x, y, z = np.genfromtxt('scripts/data1.txt', unpack=True)
+a = x * 100 /2.25
+b = y * 10 /2.2
+c = z * 10 /2.2
+cw = 385
+p = 8520
+d = 0.03
+n = 10
+ka= [0] * n
+
+def k(x, y, z):
+    return (p*cw*d**2)/(2*x*np.log(y/z))
+
+print('Messing: ')
+for i in range(n):
+	ka[i] = k(a[i],b[i],c[i])
+	print('Kappa: ',ka[i])
+ma = unp.uarray(*avg_and_sem(ka))
+print('ma: ',ma)
+
+print('Alu: ')
+x, y, z = np.genfromtxt('scripts/data2.txt', unpack=True)
+a = x * 100 /2.25
+b = y * 10 /2.5
+c = z * 10 /2.5
+cw= 830
+p= 2800
+d= 0.03
+n=10
+kb= [0] * n
+for i in range(n):
+	kb[i] = k(a[i],b[i],c[i])
+	print('Kappa: ',kb[i])
+mb = unp.uarray(*avg_and_sem(kb))
+print('mb: ',mb)
+
+print('Edelstahl: ')
+x, y, z = np.genfromtxt('scripts/data3.txt', unpack=True)
+a = x * 200 /3.3
+b = y * 10 /2.25
+c = z * 10 /2.25
+cw= 400
+r= 8000
+d= 0.03
+n= 5
+kc= [0] * n
+for i in range(n):
+	kc[i] = k(a[i],b[i],c[i])
+	print('Kappa: ',kc[i])
+mc = unp.uarray(*avg_and_sem(kc))
+print('mc: ',mc)
