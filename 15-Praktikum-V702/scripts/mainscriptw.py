d81c74df1f7b32781dc43e5a43a36a0bf7113aaa
Imigir <david.rolf@online.de>
2018-04-20 11:12:26 +0200

    test

--- 15-Praktikum-V702/scripts/mainscriptw.py
+++ 15-Praktikum-V702/scripts/mainscriptw.py
@@ -10,7 +10,9 @@ from scipy import stats
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
 import scipy.constants as const
+import math as ma
 
 # BackwardsVNominal = []
 # BackwardsVStd = []
@@ -44,7 +46,34 @@ import scipy.constants as const
 # plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 # plt.savefig('build/'+'VgegenDeltaV')
 
-# a = unp.uarray(params[0], np.sqrt(covar[0][0]))
-# params = unp.uarray(params, np.sqrt(np.diag(covar)))
-# makeNewTable([convert((r'$c_\text{1}$',r'$c_\text{2}$',r'$T_{\text{A}1}$',r'$T_{\text{A}2}$',r'$\alpha$',r'$D_1$',r'$D_2$',r'$A_1$',r'$A_2$',r'$A_3$',r'$A_4$'),strFormat),convert(np.array([paramsGes2[0],paramsGes1[0],deltat2*10**6,deltat1*10**6,-paramsDaempfung[0]*2,4.48*10**-6 *paramsGes1[0]/2*10**3, 7.26*10**-6 *paramsGes1[0]/2*10**3, (VierteMessung-2*deltat2*10**6)[0]*10**-6 *1410 /2*10**3, unp.uarray((VierteMessung[1]-VierteMessung[0])*10**-6 *1410 /2*10**3, 0), unp.uarray((VierteMessung[2]-VierteMessung[1])*10**-6 *2500 /2*10**3, 0),unp.uarray((VierteMessung[3]-VierteMessung[2])*10**-6 *1410 /2*10**3, 0)]),unpFormat,[[r'\meter\per\second',"",True],[r'\meter\per\second',"",True],[r'\micro\second',"",True],[r'\micro\second',"",True],[r'\per\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'2.2f',True]]),convert(np.array([2730,2730]),floatFormat,[r'\meter\per\second','1.0f',True])+convert((r'-',r'-'),strFormat)+convert(unp.uarray([57,6.05,9.9],[2.5,0,0]),unpFormat,[[r'\per\meter',"",True],[r'\milli\meter',r'1.2f',True],[r'\milli\meter',r'1.2f',True]])+convert((r'-',r'-',r'-',r'-'),strFormat),convert(np.array([(2730-paramsGes2[0])/2730*100,(2730-paramsGes1[0])/2730*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-'),strFormat)+convert(np.array([(-paramsDaempfung[0]*2-unp.uarray(57,2.5))/unp.uarray(57,2.5)*100,(4.48*10**-6 *paramsGes1[0]/2*10**3-6.05)/6.05*100, (-7.26*10**-6 *paramsGes1[0]/2*10**3+9.90)/9.90*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-',r'-',r'-'),strFormat)],r'{Wert}&{gemessen}&{Literaturwert\cite{cAcryl},\cite{alphaAcryl}}&{Abweichung}','Ergebnisse', ['c ','c',r'c','c'])
 
+N,t = np.genfromtxt('scripts/data1.txt', unpack = True)
+N_0 = 223/900
+N_err = np.sqrt(N)/30
+N = N/30-N_0
+for i in range(0,len(N)):
+	if N[i]<0:
+		N[i]=0
+N = unp.uarray(N, N_err)
+N = unp.log(N)
+t = t*30
+
+#a)
+paramsLinear, errorsLinear, sigma_y = linregress(t, noms(N))
+
+steigung = unp.uarray(paramsLinear[0], errorsLinear[0])
+achsenAbschnitt = unp.uarray(paramsLinear[1], errorsLinear[1])
+
+print('Steigung=', steigung)
+print('Achsenabschnitt=', achsenAbschnitt)
+
+plt.cla()
+plt.clf()
+x_plot = np.linspace(0,300*30)
+plt.errorbar(t, noms(N), yerr=stds(N), fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.plot(x_plot, x_plot*paramsLinear[0]+paramsLinear[1], 'b-', linewidth=0.8, label='Ausgleichsgerade des Plateaus')
+plt.xlabel(r'$t/\si{\second}$')
+plt.ylabel(r'$N/\si{\becquerel}$')
+plt.xlim(0,300*30)
+plt.legend(loc="best")
+plt.savefig('content/images/Graph1.1.pdf')
