6aab42076714c31bfac6f3fa7f759940b183abe0
Imigir <david.rolf@online.de>
2018-05-05 00:54:52 +0200

    Korrektur V702

--- 15-Praktikum-V702/scripts/mainscriptw.py
+++ 15-Praktikum-V702/scripts/mainscriptw.py
@@ -50,17 +50,22 @@ plt.savefig('build/'+'VgegenDeltaV')
 #a)
 #1 Vanadium
 t,N = np.genfromtxt('scripts/data1.txt', unpack = True)
+N1 = N[N>8]
 N_0 = 223/900
 N_err = np.sqrt(N-N_0*30)/30
+N_err2 = np.sqrt(N1-N_0*30)/30
 N = N/30-N_0
-N_log = np.log(N)
-N_log_err = [np.log(N+N_err)-np.log(N), np.log(N)-np.log(N-N_err)]
+N1 = N1/30-N_0
+N_log = np.log(N1)
+N_log_err = [np.log(N1+N_err2)-np.log(N1), np.log(N1)-np.log(N1-N_err2)]
 N = unp.uarray(N, N_err)
+N1 = unp.uarray(N1, N_err2)
 t = t*30
+t1 = t[t!=22*30]
 
 #print('N=', N)
 
-paramsLinear, errorsLinear, sigma_y = linregress(t, N_log)
+paramsLinear, errorsLinear, sigma_y = linregress(t1, N_log)
 
 steigung = unp.uarray(paramsLinear[0], errorsLinear[0])
 achsenAbschnitt = unp.uarray(paramsLinear[1], errorsLinear[1])
@@ -73,7 +78,7 @@ print('tau1=', -np.log(2)/steigung)
 plt.cla()
 plt.clf()
 x_plot = np.linspace(0,30*30+20)
-plt.errorbar(t, N_log, yerr=[N_log_err[0],N_log_err[1]], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.errorbar(t1, N_log, yerr=[N_log_err[0],N_log_err[1]], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
 plt.plot(x_plot, x_plot*paramsLinear[0]+paramsLinear[1], 'k-', linewidth=0.8, label='Ausgleichsgerade')
 plt.xlabel(r'$t/\si{\second}$')
 plt.ylabel(r'$\ln(N/\si{\becquerel})$')
@@ -84,7 +89,8 @@ plt.savefig('content/images/VanadiumLog.pdf')
 plt.cla()
 plt.clf()
 x_plot = np.linspace(0,30*30+20)
-plt.errorbar(t, noms(N), yerr=stds(N), fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.errorbar(t1, noms(N1), yerr=stds(N1), fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.errorbar(t[21], noms(N)[21], yerr=stds(N)[21], color='grey', fmt='x', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='lightgrey',barsabove=True ,label='ungenutzter Messwert')
 plt.plot(x_plot, np.exp(x_plot*paramsLinear[0]+paramsLinear[1]), 'k-', linewidth=0.8, label='Ausgleichskurve')
 plt.xlabel(r'$t/\si{\second}$')
 plt.ylabel(r'$N/\si{\becquerel}$')
@@ -93,18 +99,19 @@ plt.legend(loc="best")
 plt.savefig('content/images/Vanadium.pdf')
 
 
-makeTable([t, noms(N), stds(N), N_log, N_log_err[0], N_log_err[1]], r'{'+r'$t/\si{\second}$'+r'} & {'+r'$N_.V/\si{\becquerel}$'+r'} & {'+r'$\sigma_{N_.V}/\si{\becquerel}$'+r'} & {'+r'$\ln\left(N_.V/\si{\becquerel}\right)$'+r'} & {'+r'$\ln\left(\frac{N_.V+\sigma_{N_.V}}{N}\right)$'+r'} & {'+r'$\ln\left(\frac{N_.V}{N_.V-\sigma_{N_.V}}\right)$'+r'}', 'tabVanadium', ['S[table-format=3.0]', 'S[table-format=1.1]', 'S[table-format=1.1]', 'S[table-format=1.2]', 'S[table-format=1.2]', 'S[table-format=1.2]'], ["%3.0f", "%1.1f", "%1.1f", "%1.2f", "%1.2f", "%1.2f"])
+makeTable([t1, noms(N1), stds(N1), N_log, N_log_err[0], N_log_err[1]], r'{'+r'$t/\si{\second}$'+r'} & {'+r'$N_.V/\si{\becquerel}$'+r'} & {'+r'$\sigma_{N_.V}/\si{\becquerel}$'+r'} & {'+r'$\ln\left(N_.V/\si{\becquerel}\right)$'+r'} & {'+r'$\left|\ln\left(\frac{N_.V+\sigma_{N_.V}}{N}\right)\right|$'+r'} & {'+r'$\left|\ln\left(\frac{N_.V}{N_.V-\sigma_{N_.V}}\right)\right|$'+r'}', 'tabVanadium', ['S[table-format=3.0]', 'S[table-format=1.1]', 'S[table-format=1.1]', 'S[table-format=1.2]', 'S[table-format=1.2]', 'S[table-format=1.2]'], ["%3.0f", "%1.1f", "%1.1f", "%1.2f", "%1.2f", "%1.2f"])
 
 #2 Rhodium
 t2,N2 = np.genfromtxt('scripts/data2.txt', unpack = True)
 N2_err = np.sqrt(N2-N_0*10)/10
 N2 = N2/10-N_0
-N2_log = np.log(N2)
-N2_log_err = [np.log(N2+N2_err)-np.log(N2), np.log(N2)-np.log(N2-N2_err)]
+N2_log = np.log(N2[:44])
+N2_err2 = N2-N2_err
+N2_log_err = [np.log(N2[:44]+N2_err[:44])-np.log(N2[:44]), np.log(N2[:44])-np.log(N2_err2[:44])]
 N2 = unp.uarray(N2, N2_err)
 t2 = t2*10
 
-paramsLinear2, errorsLinear2, sigma_y = linregress(t2[25:45], N2_log[25:45])
+paramsLinear2, errorsLinear2, sigma_y = linregress(t2[25:44], N2_log[25:44])
 steigung2 = unp.uarray(paramsLinear2[0], errorsLinear2[0])
 achsenAbschnitt2 = unp.uarray(paramsLinear2[1], errorsLinear2[1])
 
@@ -114,6 +121,8 @@ achsenAbschnitt1 = unp.uarray(paramsLinear1[1], errorsLinear1[1])
 
 x_plot = np.linspace(0,60*10+10)
 N_t = np.exp(x_plot*paramsLinear1[0]+paramsLinear1[1])+np.exp(x_plot*paramsLinear2[0]+paramsLinear2[1])
+N21 = N2[0:20]-np.exp(t2[0:20]*paramsLinear2[0]+paramsLinear2[1])
+N21_log_err = [np.log(noms(N21)+stds(N21))-np.log(noms(N21)), np.log(noms(N21))-np.log(noms(N21)-stds(N21))]
 
 print('Lambda21=', -steigung1)
 print('N021=', unp.exp(achsenAbschnitt1))
@@ -124,7 +133,27 @@ print('tau22=', -np.log(2)/steigung2)
 
 plt.cla()
 plt.clf()
-plt.errorbar(t2, N2_log, yerr=[N2_log_err[0],N2_log_err[1]], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.errorbar(t2[25:44], N2_log[25:44], yerr=[N2_log_err[0][25:44],N2_log_err[1][25:44]], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.plot(x_plot, x_plot*paramsLinear2[0]+paramsLinear2[1], 'orange', linewidth=0.8, label='Ausgleichsgerade2')
+plt.xlabel(r'$t/\si{\second}$')
+plt.ylabel(r'$\ln(N_\mathrm{Rh_\mathrm{104i}}/\si{\becquerel})$')
+plt.xlim(25*10,44*10+10)
+plt.legend(loc="best")
+plt.savefig('content/images/RhodiumLog2.pdf')
+
+plt.cla()
+plt.clf()
+plt.errorbar(t2[0:20], np.log(noms(N2)[0:20]-np.exp(t2[0:20]*paramsLinear2[0]+paramsLinear2[1])), yerr=[N21_log_err[0][0:20],N21_log_err[1][0:20]], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.plot(x_plot, x_plot*paramsLinear1[0]+paramsLinear1[1], linewidth=0.8, label='Ausgleichsgerade1')
+plt.xlabel(r'$t/\si{\second}$')
+plt.ylabel(r'$\ln((N-N_\mathrm{Rh_\mathrm{104i}})/\si{\becquerel})$')
+plt.xlim(0,20*10+10)
+plt.legend(loc="best")
+plt.savefig('content/images/RhodiumLog1.pdf')
+
+plt.cla()
+plt.clf()
+plt.errorbar(t2[0:44], N2_log[0:44], yerr=[N2_log_err[0][0:44],N2_log_err[1][0:44]], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
 plt.plot(x_plot, x_plot*paramsLinear1[0]+paramsLinear1[1], linewidth=0.8, label='Ausgleichsgerade1')
 plt.plot(x_plot, x_plot*paramsLinear2[0]+paramsLinear2[1], linewidth=0.8, label='Ausgleichsgerade2')
 plt.plot(x_plot, np.log(N_t), 'k-', linewidth=0.8, label='N(t)')
@@ -132,18 +161,19 @@ plt.xlabel(r'$t/\si{\second}$')
 plt.ylabel(r'$\ln(N/\si{\becquerel})$')
 plt.xlim(0,44*10+10)
 plt.legend(loc="best")
-plt.savefig('content/images/RhodiumLog.pdf')
+plt.savefig('content/images/RhodiumLog3.pdf')
 
 plt.cla()
 plt.clf()
 x_plot = np.linspace(0,60*10+10)
-plt.errorbar(t2, noms(N2), yerr=stds(N2), fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.errorbar(t2[:44], noms(N2)[:44], yerr=stds(N2)[:44], fmt='rx', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='g',barsabove=True ,label='Messwerte')
+plt.errorbar(t2[44:], noms(N2)[44:], yerr=stds(N2)[44:], color='grey', fmt='x', markersize=5, elinewidth=0.5, capsize=2, capthick=0.5, ecolor='lightgrey',barsabove=True ,label='ungenutzte Messwerte')
 plt.plot(x_plot, N_t, 'k-', linewidth=0.8, label='N(t)')
 plt.xlabel(r'$t/\si{\second}$')
 plt.ylabel(r'$N/\si{\becquerel}$')
-plt.xlim(0,44*10+10)
+plt.xlim(0,60*10+10)
 plt.legend(loc="best")
 plt.savefig('content/images/Rhodium.pdf')
 
-makeTable([t2[0:29], noms(N2)[0:29], stds(N2)[0:29], N2_log[0:29], N2_log_err[0][0:29], N2_log_err[1][0:29]], r'{'+r'$t/\si{\second}$'+r'} & {'+r'$N_.{Rh}/\si{\becquerel}$'+r'} & {'+r'$\sigma_{N_.{Rh}}/\si{\becquerel}$'+r'} & {'+r'$\ln\left(N_.{Rh}/\si{\becquerel}\right)$'+r'} & {'+r'$\ln\left(\frac{N_.{Rh}+\sigma_{N_.{Rh}}}{N_.{Rh}}\right)$'+r'} & {'+r'$\ln\left(\frac{N_.{Rh}}{N_.{Rh}-\sigma_{N_.{Rh}}}\right)$'+r'}', 'tabRhodium1', ['S[table-format=3.0]', 'S[table-format=1.1]', 'S[table-format=1.1]', 'S[table-format=1.2]', 'S[table-format=1.2]', 'S[table-format=1.2]'], ["%3.0f", "%1.1f", "%1.1f", "%1.2f", "%1.2f", "%1.2f"])
-makeTable([t2[29:45], noms(N2)[29:45], stds(N2)[29:45], N2_log[29:45], N2_log_err[0][29:45], N2_log_err[1][29:45]], r'{'+r'$t/\si{\second}$'+r'} & {'+r'$N_.{Rh}/\si{\becquerel}$'+r'} & {'+r'$\sigma_{N_.{Rh}}/\si{\becquerel}$'+r'} & {'+r'$\ln\left(N_.{Rh}/\si{\becquerel}\right)$'+r'} & {'+r'$\ln\left(\frac{N_.{Rh}+\sigma_{N_.{Rh}}}{N_.{Rh}}\right)$'+r'} & {'+r'$\ln\left(\frac{N_.{Rh}}{N_.{Rh}-\sigma_{N_.{Rh}}}\right)$'+r'}', 'tabRhodium2', ['S[table-format=3.0]', 'S[table-format=1.1]', 'S[table-format=1.1]', 'S[table-format=1.2]', 'S[table-format=1.2]', 'S[table-format=1.2]'], ["%3.0f", "%1.1f", "%1.1f", "%1.2f", "%1.2f", "%1.2f"])
+makeTable([t2[0:29], noms(N2)[0:29], stds(N2)[0:29], N2_log[0:29], N2_log_err[0][0:29], N2_log_err[1][0:29]], r'{'+r'$t/\si{\second}$'+r'} & {'+r'$N_.{Rh}/\si{\becquerel}$'+r'} & {'+r'$\sigma_{N_.{Rh}}/\si{\becquerel}$'+r'} & {'+r'$\ln\left(N_.{Rh}/\si{\becquerel}\right)$'+r'} & {'+r'$\left|\ln\left(\frac{N_.{Rh}+\sigma_{N_.{Rh}}}{N_.{Rh}}\right)\right|$'+r'} & {'+r'$\left|\ln\left(\frac{N_.{Rh}}{N_.{Rh}-\sigma_{N_.{Rh}}}\right)\right|$'+r'}', 'tabRhodium1', ['S[table-format=3.0]', 'S[table-format=1.1]', 'S[table-format=1.1]', 'S[table-format=1.2]', 'S[table-format=1.2]', 'S[table-format=1.2]'], ["%3.0f", "%1.1f", "%1.1f", "%1.2f", "%1.2f", "%1.2f"])
+makeTable([t2[29:44], noms(N2)[29:44], stds(N2)[29:44], N2_log[29:44], N2_log_err[0][29:44], N2_log_err[1][29:44]], r'{'+r'$t/\si{\second}$'+r'} & {'+r'$N_.{Rh}/\si{\becquerel}$'+r'} & {'+r'$\sigma_{N_.{Rh}}/\si{\becquerel}$'+r'} & {'+r'$\ln\left(N_.{Rh}/\si{\becquerel}\right)$'+r'} & {'+r'$\left|\ln\left(\frac{N_.{Rh}+\sigma_{N_.{Rh}}}{N_.{Rh}}\right)\right|$'+r'} & {'+r'$\left|\ln\left(\frac{N_.{Rh}}{N_.{Rh}-\sigma_{N_.{Rh}}}\right)\right|$'+r'}', 'tabRhodium2', ['S[table-format=3.0]', 'S[table-format=1.1]', 'S[table-format=1.1]', 'S[table-format=1.2]', 'S[table-format=1.2]', 'S[table-format=1.2]'], ["%3.0f", "%1.1f", "%1.1f", "%1.2f", "%1.2f", "%1.2f"])
--- 16-Praktikum-US1/scripts/mainscriptw.py
+++ 16-Praktikum-US1/scripts/mainscriptw.py
@@ -48,22 +48,30 @@ import scipy.constants as const
 # params = unp.uarray(params, np.sqrt(np.diag(covar)))
 # makeNewTable([convert((r'$c_\text{1}$',r'$c_\text{2}$',r'$T_{\text{A}1}$',r'$T_{\text{A}2}$',r'$\alpha$',r'$D_1$',r'$D_2$',r'$A_1$',r'$A_2$',r'$A_3$',r'$A_4$'),strFormat),convert(np.array([paramsGes2[0],paramsGes1[0],deltat2*10**6,deltat1*10**6,-paramsDaempfung[0]*2,4.48*10**-6 *paramsGes1[0]/2*10**3, 7.26*10**-6 *paramsGes1[0]/2*10**3, (VierteMessung-2*deltat2*10**6)[0]*10**-6 *1410 /2*10**3, unp.uarray((VierteMessung[1]-VierteMessung[0])*10**-6 *1410 /2*10**3, 0), unp.uarray((VierteMessung[2]-VierteMessung[1])*10**-6 *2500 /2*10**3, 0),unp.uarray((VierteMessung[3]-VierteMessung[2])*10**-6 *1410 /2*10**3, 0)]),unpFormat,[[r'\meter\per\second',"",True],[r'\meter\per\second',"",True],[r'\micro\second',"",True],[r'\micro\second',"",True],[r'\per\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'2.2f',True]]),convert(np.array([2730,2730]),floatFormat,[r'\meter\per\second','1.0f',True])+convert((r'-',r'-'),strFormat)+convert(unp.uarray([57,6.05,9.9],[2.5,0,0]),unpFormat,[[r'\per\meter',"",True],[r'\milli\meter',r'1.2f',True],[r'\milli\meter',r'1.2f',True]])+convert((r'-',r'-',r'-',r'-'),strFormat),convert(np.array([(2730-paramsGes2[0])/2730*100,(2730-paramsGes1[0])/2730*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-'),strFormat)+convert(np.array([(-paramsDaempfung[0]*2-unp.uarray(57,2.5))/unp.uarray(57,2.5)*100,(4.48*10**-6 *paramsGes1[0]/2*10**3-6.05)/6.05*100, (-7.26*10**-6 *paramsGes1[0]/2*10**3+9.90)/9.90*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-',r'-',r'-'),strFormat)],r'{Wert}&{gemessen}&{Literaturwert\cite{cAcryl},\cite{alphaAcryl}}&{Abweichung}','Ergebnisse', ['c ','c',r'c','c'])
 
+def linear(x, A):
+    return A*x
+
 #a)
 l,t,t1 = np.genfromtxt('scripts/data1.txt', unpack=True)
 l = l/100
-t=t/2000000
-paramsLinear, errorsLinear, sigma_y = linregress(t,l)
+t=t/(2*10**6)
+
+#paramsLinear, errorsLinear, sigma_y = linregress(t,l)
+#steigung = unp.uarray(paramsLinear[0], errorsLinear[0])
+#achsenAbschnitt = unp.uarray(paramsLinear[1], errorsLinear[1])
+
+paramsLinear, covar = curve_fit(linear, t, l)
+errorsLinear = np.sqrt(np.diag(covar))
 steigung = unp.uarray(paramsLinear[0], errorsLinear[0])
-achsenAbschnitt = unp.uarray(paramsLinear[1], errorsLinear[1])
 
 print('c=', steigung)
-print('x0=', unp.exp(achsenAbschnitt))
+print('Abweichung in %:', (paramsLinear[0]-2730)/2730*100)
 
 plt.cla()
 plt.clf()
 x_plot = np.linspace(0,50)
-plt.plot(t*1000000, l*100, 'rx', linewidth=0.8, label='Messwerte')
-plt.plot(x_plot, x_plot/10000*paramsLinear[0]+paramsLinear[1], 'k-', linewidth=0.8, label='Ausgleichsgerade')
+plt.plot(t*10**6, l*100, 'rx', linewidth=0.8, label='Messwerte')
+plt.plot(x_plot, x_plot/10000*paramsLinear[0], 'k-', linewidth=0.8, label='Ausgleichsgerade')
 plt.xlabel(r'$t/10^{-6}\si{\second}$')
 plt.ylabel(r'$l/10^{-2}\si{\metre}$')
 plt.xlim(0,50)
@@ -72,4 +80,4 @@ plt.legend(loc="best")
 plt.savefig('content/images/Schallgeschwindigkeit.pdf')
 
 
-makeTable([t1, 0.3+t*2000000, t*2000000, t*1000000, l], r'{'+r'$t_.1/10^{-6}\si{\second}$'+r'} & {'+r'$t_.2/10^{-6}\si{\second}$'+r'} & {'+r'$\Delta t_.{mess}/10^{-6}\si{\second}$'+r'} & {'+r'$\Delta t_.{eff}/\si{\second}$'+r'} & {'+r'$l/10^{-2}\si{\metre}$'+r'}', 'tabSchallgeschwindigkeit', ['S[table-format=1.1]', 'S[table-format=2.1]', 'S[table-format=2.1]', 'S[table-format=2.2]', 'S[table-format=2.2]'], ["%1.1f", "%2.1f", "%2.1f", "%2.2f", "%2.2f"])
+makeTable([t1, 0.3+t*2*10**6, t*2*10**6, t*10**6, l], r'{'+r'$t_.1/10^{-6}\si{\second}$'+r'} & {'+r'$t_.2/10^{-6}\si{\second}$'+r'} & {'+r'$\Delta t_.{mess}/10^{-6}\si{\second}$'+r'} & {'+r'$\Delta t_.{eff}/\si{\second}$'+r'} & {'+r'$l/10^{-2}\si{\metre}$'+r'}', 'tabSchallgeschwindigkeit', ['S[table-format=1.1]', 'S[table-format=2.1]', 'S[table-format=2.1]', 'S[table-format=2.2]', 'S[table-format=2.2]'], ["%1.1f", "%2.1f", "%2.1f", "%2.2f", "%2.2f"])
