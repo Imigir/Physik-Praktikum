ce85599a70718dceb2f701a00da7c140d1accfb5
Imigir <david.rolf@online.de>
2020-02-03 14:34:16 +0100

    data

---
+++ FP-V46/scripts/linregress.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+import uncertainties.unumpy as unp
+
+
+def linregress(x, y):
+	assert len(x) == len(y)
+
+
+	N = len(y)
+	Delta = N * sum(x**2) - (sum(x))**2
+
+	A = (N * sum(x * y) - sum(x) * sum(y)) / Delta
+	B = (sum(x**2) * sum(y) - sum(x) * sum(x * y)) / Delta
+
+	sigma_y = unp.sqrt(sum((y - A * x - B)**2) / (N - 2))
+
+	A_error = sigma_y * unp.sqrt(N / Delta)
+	B_error = sigma_y * unp.sqrt(sum(x**2) / Delta)
+	
+	y_quer = sum(y)/(N-1)
+	R = sum((A*x+B-y_quer)**2)/sum((y-y_quer)**2) #Bestimmtheitsmaß
+
+	return [A, B], [A_error, B_error], R
