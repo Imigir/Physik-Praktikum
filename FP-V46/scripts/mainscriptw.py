257e58f4e95414e0ea4ed0935302ce88f65eb9ab
David Rolf <david.rolf@online.de>
2020-02-03 19:55:01 +0100

    auswertung 1

--- FP-V46/scripts/mainscriptw.py
+++ FP-V46/scripts/mainscriptw.py
@@ -50,212 +50,135 @@ import scipy.constants as const
 # makeNewTable([convert((r'$c_\text{1}$',r'$c_\text{2}$',r'$T_{\text{A}1}$',r'$T_{\text{A}2}$',r'$\alpha$',r'$D_1$',r'$D_2$',r'$A_1$',r'$A_2$',r'$A_3$',r'$A_4$'),strFormat),convert(np.array([paramsGes2[0],paramsGes1[0],deltat2*10**6,deltat1*10**6,-paramsDaempfung[0]*2,4.48*10**-6 *paramsGes1[0]/2*10**3, 7.26*10**-6 *paramsGes1[0]/2*10**3, (VierteMessung-2*deltat2*10**6)[0]*10**-6 *1410 /2*10**3, unp.uarray((VierteMessung[1]-VierteMessung[0])*10**-6 *1410 /2*10**3, 0), unp.uarray((VierteMessung[2]-VierteMessung[1])*10**-6 *2500 /2*10**3, 0),unp.uarray((VierteMessung[3]-VierteMessung[2])*10**-6 *1410 /2*10**3, 0)]),unpFormat,[[r'\meter\per\second',"",True],[r'\meter\per\second',"",True],[r'\micro\second',"",True],[r'\micro\second',"",True],[r'\per\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'2.2f',True]]),convert(np.array([2730,2730]),floatFormat,[r'\meter\per\second','1.0f',True])+convert((r'-',r'-'),strFormat)+convert(unp.uarray([57,6.05,9.9],[2.5,0,0]),unpFormat,[[r'\per\meter',"",True],[r'\milli\meter',r'1.2f',True],[r'\milli\meter',r'1.2f',True]])+convert((r'-',r'-',r'-',r'-'),strFormat),convert(np.array([(2730-paramsGes2[0])/2730*100,(2730-paramsGes1[0])/2730*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-'),strFormat)+convert(np.array([(-paramsDaempfung[0]*2-unp.uarray(57,2.5))/unp.uarray(57,2.5)*100,(4.48*10**-6 *paramsGes1[0]/2*10**3-6.05)/6.05*100, (-7.26*10**-6 *paramsGes1[0]/2*10**3+9.90)/9.90*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-',r'-',r'-'),strFormat)],r'{Wert}&{gemessen}&{Literaturwert\cite{cAcryl},\cite{alphaAcryl}}&{Abweichung}','Ergebnisse', ['c ','c',r'c','c'])
 
 
-#Erdmagnetfeld
-def B_field(I,N,R):
-	return const.mu_0 * 8/np.sqrt(125)* I*N/R
+#magnetfeld
+B_max = 213*10**(-3) #T
+#B_max = B_max*1.75
+print('max B field [mT]:', B_max*10**3)
+z,B = np.genfromtxt('scripts/magnetfeld.txt',unpack=True) # mm, mT
+#B = B*1.75
 
-N_V = 20
-R_V = 11.735*10**(-2) # m
-I_V = 229*10**(-3) # A
-print('vertical B field:', B_field(I_V,N_V,R_V))
+plt.cla()
+plt.clf()
+plt.plot(z,B,'rx')
+plt.xlabel(r'$z/\si{\milli\metre}$')
+plt.ylabel(r'$B/\si{\milli\tesla}$')
+#plt.xlim(-30,30)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+#plt.legend(loc='best')
+plt.savefig('build/magnetfeld.pdf')
 
-v, I_S1, I_S2, I_H1, I_H2 = np.genfromtxt('scripts/horizontal.txt',unpack=True) # kHz,mA
-v = v*1000 # Hz
-I_S1 = (I_S1+12)*10**(-3) # A
-I_S2 = (I_S2+12)*10**(-3) # A
-I_H1 = I_H1*10**(-3) # A
-I_H2 = I_H2*10**(-3) # A
-R_S = 16.39*10**(-2) # m
-N_S = 11
-R_H = 15.79*10**(-2) # m
-N_H = 154
 
-B_S1 = B_field(I_S1,N_S,R_S)
-B_S2 = B_field(I_S2,N_S,R_S)
-B_H1 = B_field(I_H1,N_H,R_H)
-B_H2 = B_field(I_H2,N_H,R_H)
+#reinprobe
+print('reinprobe')
+l,t1,t2 = np.genfromtxt('scripts/reinprobe.txt',unpack=True) # micrometer, degree
+tr = (t1-t2)/2
+tr = 2*np.pi*tr/360 # rad
+tr = tr/5.11*10**3 #rad/m
 
-B_1 = B_H1+B_S1
-B_2 = B_H2+B_S2
+def hyperbel(l,a):
+	return a/l**2
 
-makeTable([v/1000,I_S1*1000,I_H1*1000,B_S1*10**6,B_H1*10**6,B_1*10**6], r'{$\nu/\si{\kilo\hertz}$} & {$I_\text{S,A}/\si{\milli\ampere}$} & {$I_\text{H,A}/\si{\milli\ampere}$} & {$B_\text{S,A}/\si{\micro\tesla}$} & {$B_\text{H,A}/\si{\micro\tesla}$} & {$B_\text{Ges,A}/\si{\micro\tesla}$}','messung1A', ['S[table-format=4.0]','S[table-format=3.0]','S[table-format=3.0]','S[table-format=3.2]','S[table-format=3.2]','S[table-format=3.2]'], ["%4.0f", "%3.0f", "%3.0f", "%3.2f", "%3.2f", "%3.2f"])
-makeTable([v/1000,I_S2*1000,I_H2*1000,B_S2*10**6,B_H2*10**6,B_2*10**6], r'{$\nu/\si{\kilo\hertz}$} & {$I_\text{S,B}/\si{\milli\ampere}$} & {$I_\text{H,B}/\si{\milli\ampere}$} & {$B_\text{S,B}/\si{\micro\tesla}$} & {$B_\text{H,B}/\si{\micro\tesla}$} & {$B_\text{Ges,B}/\si{\micro\tesla}$}','messung1B', ['S[table-format=4.0]','S[table-format=3.0]','S[table-format=3.0]','S[table-format=3.2]','S[table-format=3.2]','S[table-format=3.2]'], ["%4.0f", "%3.0f", "%3.0f", "%3.2f", "%3.2f", "%3.2f"])
+params, covariance_matrix = curve_fit(hyperbel,l,tr)
+errors = np.sqrt(np.diag(covariance_matrix))
 
-def Linear(x,a,b):
-	return a*x+b
+x = np.linspace(0.9,2.7,1000)
+plt.cla()
+plt.clf()
+plt.plot(l**2,tr,'rx',label=r'Messwerte')
+plt.plot(x**2,hyperbel(x,*params),'b-',label=r'Ausgleichskurve')
+plt.xlabel(r'$\lambda^2/\si{\micro\metre\squared}$')
+plt.ylabel(r'$\Delta\theta/\si{\text{rad}\per\metre}$')
+plt.xlim(0.9,7.2)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.legend(loc='best')
+plt.savefig('build/reinprobe.pdf')
 
-params1, covariance_matrix1 = curve_fit(Linear,v,B_1)
-errors1 = np.sqrt(np.diag(covariance_matrix1))
-params2, covariance_matrix2 = curve_fit(Linear,v,B_2)
-errors2 = np.sqrt(np.diag(covariance_matrix2))
+print('a=',unp.uarray(params[0],errors[0]))
+#print('b=',unp.uarray(params[1],errors[1]))
+#print('c=',unp.uarray(params[2],errors[2]))
 
-a1 = unp.uarray(params1[0],errors1[0])
-a2 = unp.uarray(params2[0],errors2[0])
+
+#dotiert
+print('dotiert')
+l,t1,t2 = np.genfromtxt('scripts/dotiert1.txt',unpack=True) # micrometer, degree
+td1 = (t1-t2)/2
+td1 = 2*np.pi*td1/360 # rad
+td1 = td1/1.36*10**3 #rad/m
+
+plt.cla()
+plt.clf()
+plt.plot(l**2,td1,'rx',label=r'Messwerte')
+plt.xlabel(r'$\lambda^2/\si{\micro\metre\squared}$')
+plt.ylabel(r'$\Delta\theta/\si{\text{rad}\per\metre}$')
+#plt.xlim(0.9,7.2)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+#plt.legend(loc='best')
+plt.savefig('build/dotiert1.pdf')
+
+l,t1,t2 = np.genfromtxt('scripts/dotiert2.txt',unpack=True) # micrometer, degree
+td2 = (t1-t2)/2
+td2 = 2*np.pi*td2/360 # rad
+td2 = td2/1.296*10**3 #rad/m
+
+plt.cla()
+plt.clf()
+plt.plot(l**2,td2,'rx',label=r'Messwerte')
+plt.xlabel(r'$\lambda^2/\si{\micro\metre\squared}$')
+plt.ylabel(r'$\Delta\theta/\si{\text{rad}\per\metre}$')
+#plt.xlim(0.9,7.2)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+#plt.legend(loc='best')
+plt.savefig('build/dotiert2.pdf')
+
+#dif
+def squared(l,a):
+	return a*l**2
+
+td1=td1-tr
+params1, covariance_matrix = curve_fit(squared,l[1:],td1[1:])
+errors1 = np.sqrt(np.diag(covariance_matrix))
+td12=td2-tr
+params2, covariance_matrix = curve_fit(squared,l[1:],td2[1:])
+errors2 = np.sqrt(np.diag(covariance_matrix))
+
+x = np.linspace(0.9,2.7,1000)
+plt.cla()
+plt.clf()
+plt.figure(figsize=(6,4))
+plt.plot(l**2,td1,'rx',label=r'Messwerte 1')
+plt.plot(l[0]**2,td1[0],'gx')
+plt.plot(x**2,squared(x,*params1),'b-',label=r'Ausgleichsgerade 1')
+plt.plot(l**2,td2,'mx',label=r'Messwerte 2')
+plt.plot(x**2,squared(x,*params2),'c-',label=r'Ausgleichsgerade 2')
+plt.plot(l[0]**2,td2[0],'gx',label=r'Ignorierte Messwerte')
+plt.xlabel(r'$\lambda^2/\si{\micro\metre\squared}$')
+plt.ylabel(r'$\Delta\theta/\si{\text{rad}\per\metre}$')
+plt.xlim(0.9,7.2)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.legend(loc='best')
+plt.savefig('build/dotiert_dif.pdf')
+
+a1=unp.uarray(params1[0],errors1[0])
+a2=unp.uarray(params2[0],errors2[0])
 print('a1 =', a1)
-print('b1 =', unp.uarray(params1[1],errors1[1]))
 print('a2 =', a2)
-print('b2 =', unp.uarray(params2[1],errors2[1]))
-
-x = np.linspace(0,1100)
-plt.cla()
-plt.clf()
-plt.plot(v/1000,B_1*10**6,'rx',label=r'$B_\text{Ges,A}$')
-plt.plot(x,Linear(x,params1[0]*1000*10**6,params1[1]*10**6),'b-',label=r'Ausgleichsgerade1')
-plt.plot(v/1000,B_2*10**6,'mx',label=r'$B_\text{Ges,B}$')
-plt.plot(x,Linear(x,params2[0]*1000*10**6,params2[1]*10**6),'c-',label=r'Ausgleichsgerade2')
-plt.xlabel(r'$\nu/\si{\kilo\hertz}$')
-plt.ylabel(r'$B/\si{\micro\tesla}$')
-plt.xlim(0,1100)
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.legend(loc='best')
-plt.savefig('build/messung1.pdf')
-
-print('horizontal B field:', avg_and_sem([params1[1],params2[1]]))
 
 
-#Lande faktoren
-def Lande(a):
-	return 4*const.pi*const.m_e/(const.e*a)
-def Kernspin(g_F):
-	return 1/2*(2.0023/g_F-1)
-	
-g_F1 = Lande(a1)
-g_F2 = Lande(a2)
-print('g_F1:', g_F1)
-print('g_F2:', g_F2)
-I1 = Kernspin(g_F1)
-I2 = Kernspin(g_F2)
-print('I1:', I1)
-print('I2:', I2)
+#massen
+def masse(a,N,B,n):
+	return unp.sqrt(const.e**3*N*B/(a*8*np.pi**2*n*const.c**3*const.epsilon_0))
 
-#Zeeman_quad
-def Zeeman_ges(g_F,B,DE,M_F):
-	return g_F*const.physical_constants["Bohr magneton"][0]*B+(g_F*B*const.physical_constants["Bohr magneton"][0])**2*(1-2*M_F)/DE
+a1 = a1*10**12
+a2 = a2*10**12
+N1 = 1.2*10**24
+N2 = 2.8*10**24
+n = 3.4
 
-def Zeeman_quad(g_F,B,DE,M_F):
-	return (g_F*B*const.physical_constants["Bohr magneton"][0])**2*(1-2*M_F)/DE
+print('m1 =', masse(a1,N1,B_max,n))
+print('m2 =', masse(a2,N2,B_max,n))
+print('m1/me =', masse(a1,N1,B_max,n)/const.m_e)
+print('m2/me =', masse(a2,N2,B_max,n)/const.m_e)
 
-print('DE_G1:', Zeeman_ges(g_F1,B_1[-1],4.53*10**(-24),1))
-print('DE_G2:', Zeeman_ges(g_F2,B_2[-1],2.01*10**(-24),1))
-
-print('DE_q1:', Zeeman_quad(g_F1,B_1[-1],4.53*10**(-24),1))
-print('DE_q2:', Zeeman_quad(g_F2,B_2[-1],2.01*10**(-24),1))
-
-
-#exponential fit
-def exp_fkt(x,a,b):
-	return (1-np.exp(-a*(x-b)))
-	
-t,t,t,x0,y0,t = np.genfromtxt('scripts/TEK0000.CSV',unpack=True,delimiter=",") 
-y0 = np.array(y0[x0>=0])
-for i in range(len(y0)-1):
-	y0[i+1] = y0[i+1]-y0[0]
-y0[0] = 0
-y0 = y0/y0[-1]
-x0 = np.array(x0[x0>=0]*1000)
-t,t,t,x1,y1,t = np.genfromtxt('scripts/TEK0002.CSV',unpack=True,delimiter=",") 
-y1 = np.array(y1[x1>=0])
-for i in range(len(y1)-1):
-	y1[i+1] = y1[i+1]-y1[0]
-y1[0] = 0
-y1 = y1/y1[-1]
-x1 = np.array(x1[x1>=0]*1000)
-t,t,t,x2,y2,t = np.genfromtxt('scripts/TEK0003.CSV',unpack=True,delimiter=",") 
-y2 = y2[x2>=0]
-x2 = x2[x2>=0]
-y2 = y2[x2<=0.1]
-x2 = x2[x2<=0.1]*1000
-for i in range(len(y2)-1):
-	y2[i+1] = y2[i+1]-y2[0]
-y2[0] = 0
-y2 = y2/y2[-1]
-
-params0, covariance_matrix0 = curve_fit(exp_fkt,x0,y0,p0=(0.1,0))
-errors0 = np.sqrt(np.diag(covariance_matrix0))
-print('a0 =', unp.uarray(params0[0],errors0[0]))
-print('b0 =', unp.uarray(params0[1],errors0[1]))
-params1, covariance_matrix1 = curve_fit(exp_fkt,x1,y1,p0=(0.1,0))
-errors1 = np.sqrt(np.diag(covariance_matrix1))
-print('a1 =', unp.uarray(params1[0],errors1[0]))
-print('b1 =', unp.uarray(params1[1],errors1[1]))
-params2, covariance_matrix2 = curve_fit(exp_fkt,x2,y2,p0=(0.1,0))
-errors2 = np.sqrt(np.diag(covariance_matrix2))
-print('a2 =', unp.uarray(params2[0],errors2[0]))
-print('b2 =', unp.uarray(params2[1],errors2[1]))
-
-x = np.linspace(-1,105,1000)
-plt.cla()
-plt.clf()
-plt.plot(x0,y0,'r-',label=r'Messwerte')
-plt.plot(x,(exp_fkt(x,*params0)),'b-',label=r'Ausgleichskurve')
-plt.xlabel(r'$t/\si{\milli\second}$')
-plt.ylabel(r'Transparenz')
-plt.xlim(-1,80)
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.legend(loc='best')
-plt.savefig('build/image0.pdf')
-
-plt.cla()
-plt.clf()
-plt.plot(x1,y1,'r-',label=r'Messwerte')
-plt.plot(x,(exp_fkt(x,*params1)),'b-',label=r'Ausgleichskurve')
-plt.xlabel(r'$t/\si{\milli\second}$')
-plt.ylabel(r'Transparenz')
-plt.xlim(-1,60)
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.legend(loc='best')
-plt.savefig('build/image1.pdf')
-
-plt.cla()
-plt.clf()
-plt.plot(x2,y2,'r-',label=r'Messwerte')
-plt.plot(x,(exp_fkt(x,*params2)),'b-',label=r'Ausgleichskurve')
-plt.xlabel(r'$t/\si{\milli\second}$')
-plt.ylabel(r'Transparenz')
-plt.xlim(-1,100)
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.legend(loc='best')
-plt.savefig('build/image2.pdf')
-
-
-#Oszillationen
-print('Oszillationen')
-def hyper(x,a,b,c):
-	return a+b/(x-c)
-	
-v, T1, T2 = np.genfromtxt('scripts/oszillationen.txt',unpack=True) # V,micro s
-T1 = T1*10**(-3) #ms
-T2 = T2*10**(-3) #ms
-
-params1, covariance_matrix1 = curve_fit(hyper,v,T1)
-errors1 = np.sqrt(np.diag(covariance_matrix1))
-print('a1 =', unp.uarray(params1[0],errors1[0]))
-b1 = unp.uarray(params1[1],errors1[1])
-print('b1 =', b1)
-print('c1 =', unp.uarray(params1[2],errors1[2]))
-params2, covariance_matrix2 = curve_fit(hyper,v,T2)
-errors2 = np.sqrt(np.diag(covariance_matrix2))
-print('a2 =', unp.uarray(params2[0],errors2[0]))
-b2 = unp.uarray(params2[1],errors2[1])
-print('b2 =', b2)
-print('c2 =', unp.uarray(params2[2],errors2[2]))
-
-x = np.linspace(1,11,1000)
-plt.cla()
-plt.clf()
-plt.plot(v,T1,'rx',label=r'Messwerte Resonanz1')
-plt.plot(x,(hyper(x,*params1)),'b-',label=r'Ausgleichskurve')
-plt.plot(v,T2,'mx',label=r'Messwerte Resonanz2')
-plt.plot(x,(hyper(x,*params2)),'c-',label=r'Ausgleichskurve2')
-plt.xlabel(r'$U/\si{\volt}$')
-plt.ylabel(r'$T\si{milli\second}$')
-plt.xlim(1,11)
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.legend(loc='best')
-plt.savefig('build/oszillationen.pdf')
-
-print('Verhältnis:', b1/b2)
-makeTable([v,T1,T2], r'{$U/\si{\volt}$} & {$T_1/\si{\milli\second}$} & {$T_2/\si{\milli\second}$} ','oszillationen', ['S[table-format=1.0]','S[table-format=1.2]','S[table-format=1.2]'], ["%1.0f", "%1.2f", "%1.2f"])
 
 
 
